1d6c4e8801a74f9842e28828ddf744a9a083c4ab
S Das <d.soubhik@hotmail.com>
2023-06-15 16:55:36 +0530

    Create custom-script.js

---
+++ ap/custom-script.js
@@ -0,0 +1,18 @@
+
+// When the page is fully loaded
+document.addEventListener('DOMContentLoaded', function() {
+ 
+  
+    // Handle form submission
+    const form = document.getElementById('applicationForm');
+    form.addEventListener('submit', function(event) {
+      event.preventDefault(); // Prevent form submission
+      submitForm();
+    });
+  
+    // Function to submit the form using AJAX
+    function submitForm() {
+      
+    }
+  });
+  
